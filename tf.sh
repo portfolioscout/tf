8cc003a9a3e52ad560840effbfd2edd1bbd63471
PortfolioScout <portfolioscout@gmail.com>
2016-05-14 23:10:19 -0700

    fix

--- tf.sh
+++ tf.sh
@@ -1,3 +1,3 @@
 #docker run -v /home/ruslan/tf:/tf -p 8888:8888 -it b.gcr.io/tensorflow/tensorflow /bin/sh -c 'cd /tf/nb;ipython notebook'
-docker run -v ~/tf:/tf -p 8888:8888 -it $1 /bin/sh -c 'cd /tf/nb;ipython notebook --no-browser --ip="*"'
+docker run -v ~/tf:/conda -p 8888:8888 -it $1 /bin/sh -c 'cd /conda/nb;ipython notebook --no-browser --ip="*"'
 
